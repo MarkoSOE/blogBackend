62612425e285bd2c9d5a70535aa74eaeb25ce174
Marko Alhamadani <markoalhamadani@markos-mbp.home>
2023-09-09 00:32:19 -0400

    implement token auth

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require("jsonwebtoken");
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getTokenFrom = (require) => {
+	const authorization = router.get("authorization");
+	if (authorization && authorization.startsWith("Bearer ")) {
+		return authorization.replace("Bearer ", "");
+	}
+	return null;
+};
 
 blogRouter.get("/", async (request, response) => {
 	const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -8,9 +17,15 @@ blogRouter.get("/", async (request, response) => {
 });
 
 blogRouter.post("/", async (request, response, next) => {
-	const body = request.body || request.query;
+	const body = request.body;
 
-	const user = await User.find({}).populate("blogs");
+	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: "token invalid" });
+	}
+
+	const user = await User.findbyId(decodedToken.id).populate("blogs");
 
 	console.log("users", user);
 	console.log("first user", user[0]);
